a6682c00f7e8f0f5863156401cbe002585901e84
Divyansh sharma <divyanshsharmakotarajasthan@gmail.com>
2025-09-05 23:56:07 +0530

    Add todo

--- backend/app/api.py
+++ backend/app/api.py
@@ -15,7 +15,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+todos = [{"id": "1", "item": "Read a book."}, {"id": "2", "item": "Cycle around town."}]
+
 
 @app.get("/", tags=["root"])
 async def read_root() -> dict:
     return {"message": "Welcome to your todo list."}
+
+
+@app.get("/todos", tags=["todos"])
+async def get_todos() -> dict:
+    return {"data": todos}
+
+
+@app.post("/todo", tags=["todos"])
+async def add_todo(todo: dict) -> dict:
+    todos.append(todo)
+    return {"data": {"Todo added."}}
